8643d2bbc1ebab09ba3a46fcabe616f40666ff05
timofey1981 <timofey1981@gmail.com>
2018-10-04 18:10:14 +0300

    backend init

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "off",
+        "indent": 0,
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
